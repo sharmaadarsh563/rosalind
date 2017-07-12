2cdcc752cc308f1f6a9b79f647e3e56fa21a71c8
Adarsh Sharma <sharmaadarsh563@gmail.com>
2017-07-13 00:50:05 +0530

    minor_programming_changes

--- PROB.py
+++ PROB.py
@@ -1,15 +1,19 @@
 import math
-dna = raw_input()
-gc_conts = map(float, raw_input().split(' '))
-ans = [0.0]*len(gc_conts)
 
-for i in range(len(gc_conts)):
-	gc_cont = gc_conts[i]/2
-	at_cont = (1 - gc_conts[i])/2
-	for j in range(len(dna)):
-		if dna[j] in ['A', 'T']:
-			ans[i] += math.log(at_cont, 10)
-		else:
-			ans[i] += math.log(gc_cont, 10)
+def find_prob_of_seq(dna, gc_conts):
+	ans = [0.0]*len(gc_conts)
+	for i in range(len(gc_conts)):
+		gc_cont = gc_conts[i]/2
+		at_cont = (1 - gc_conts[i])/2
+		for j in range(len(dna)):
+			if dna[j] in ['A', 'T']:
+				ans[i] += math.log(at_cont, 10)
+			else:
+				ans[i] += math.log(gc_cont, 10)
 
-print ans
+	return ans
+
+if __name__ == "__main__":
+	dna = raw_input()
+	gc_conts = map(float, raw_input().split(' '))
+	print find_prob_of_seq(dna, gc_conts)
